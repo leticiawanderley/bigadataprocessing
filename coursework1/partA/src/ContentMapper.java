1abee1d2826956bc418ed6a96abe36c0299476eb
Leticia Farias Wanderley <lfw30@itl183.student.eecs.qmul.ac.uk>
2015-11-02 12:27:09 +0000

    Improved mapper

--- coursework1/partA/src/ContentMapper.java
+++ coursework1/partA/src/ContentMapper.java
@@ -10,7 +10,7 @@ public class ContentMapper extends Mapper<LongWritable, Text, Text, IntWritable>
     private final IntWritable one = new IntWritable(1);
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] line = value.toString().split(";");
-	int size = line[3].length();
+	double size = line[3].length();
 	Text clazz = new Text(String.valueOf(Math.ceil(size/5)));
 	/* is it just the size of the tweet itself, or the size of the tweet + the size of its hashtags? */
 	context.write(clazz, one);
--- coursework1/partA/src/ContentReducer.java
+++ coursework1/partA/src/ContentReducer.java
@@ -10,7 +10,6 @@ public class ContentReducer extends Reducer<Text, IntWritable, Text, IntWritable
     private IntWritable result = new IntWritable();
 
     public void reduce(Text key, Iterable<IntWritable> values, Context context)
-
               throws IOException, InterruptedException {
 
         int sum = 0;
